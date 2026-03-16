07e4c58682ce9122dcca4799c622877273dda192
Nikhil Tembhare <tembhareng@rknec.edu>
2026-03-16 23:41:24 +0530

    Made Backend Folder

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch(err => console.log(err));
+
+
+app.get("/", (req, res) => {
+    res.send("Retina Portal Backend Running");
+});
+
+
+app.get("/api/test", (req, res) => {
+    res.json({message: "API working"});
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
